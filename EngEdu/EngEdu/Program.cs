2f14bf55da895d9ad00b806b747ac81cfd7209d6
sarunyoo <58030229@kmitl.ac.th>
2017-10-16 15:01:22 +0700

    Year 2560 semester 1

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -13,10 +13,13 @@ namespace EngEdu
             Console.WriteLine("Year 255x semester 1, I am an EngEdu freshman.");
             Console.WriteLine("Year 255x semester 2, I learn to program in C sharp.");
             Console.WriteLine("Year 2560 semester 1, I start my program life.");
+
+
             Console.WriteLine("Year 2558 semester 1, I learn to Calculus very hard.");
             Console.WriteLine("Year 2558 semester 2, I learn to Computer Progamming with Dr.Sushin.");
             Console.WriteLine("Year 2559 semester 1, I select Computer sec.");
             Console.WriteLine("Year 2559 semester 2, I got a grad F in Digital Subject.");
+            Console.WriteLine("Year 2560 semester 1, I learn to program in C++ and C sharp");
         }
     }
 }
