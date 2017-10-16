fe70012d9b5b4f59b1250b9d73fd39fc763a6f9c
sarunyoo <58030229@kmitl.ac.th>
2017-10-16 14:57:35 +0700

    Year 2559 semester 2

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -16,6 +16,7 @@ namespace EngEdu
             Console.WriteLine("Year 2558 semester 1, I learn to Calculus very hard.");
             Console.WriteLine("Year 2558 semester 2, I learn to Computer Progamming with Dr.Sushin.");
             Console.WriteLine("Year 2559 semester 1, I select Computer sec.");
+            Console.WriteLine("Year 2559 semester 2, I got a grad F in Digital Subject.");
         }
     }
 }
